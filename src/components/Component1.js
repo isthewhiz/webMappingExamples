0dd48b0be0720763c5ca156be0cff5cb64e1d6f7
isnardo gandarilla <48775290+isthewhiz@users.noreply.github.com>
2020-02-21 22:19:14 -0800

    Add files via upload

---
+++ src/components/Component1.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {Spring} from 'react-spring/renderprops';
+
+
+export default function Component1() {
+    return (
+        <Spring
+          from={{opacity: 0, marginTop: -500}} 
+          to={{ opacity: 1, marginTop: 0}}
+        >
+            
+            {props => (
+               <div style={props}>
+               <div style = {c1Style}>
+                 <h1>Component1</h1>
+                     <p>
+                         Lorem ipsum dolor sit, amet
+                         consectetur adipisicing elite. Possimus
+                         nobis adipisci eum minima desernut at porro, vertatis officia commodi itaque 
+                         vouptates vel suscipit assumenda soluta 
+                         ipsa voluptatibus laudantium labore harum?
+                    </p>
+                    <Spring
+                    from={{ number: 0 }}
+                    to={{ number: 10 }}
+                    config={{ duration: 1000}}
+                    >
+                        {props => (
+                            <div style={props}>
+                                <h1 style={counter}>
+                                    {props.number.toFixed()}
+                                </h1>
+                            </div>
+                        )}
+                    </Spring>
+                </div>
+             
+            </div>
+            )}
+        </Spring>
+      
+    );
+    
+}
+const c1Style = {
+    background: 'steelblue',
+    color: 'white',
+    padding: '1.5rem'
+};
+
+const counter ={
+    background: '#333',
+    textAlign: 'center',
+    width: '100px',
+    borderRadius: '50%',
+    margin: '1rem auto'
+};
---
+++ src/components/Component3.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Component3() {
+    return (
+        <div style={c3Style}>
+            <h1>Component3</h1>
+                   <p>Lorem ipsum dolor sit, amet
+                      consectetur adipisicing elite. Possimus
+                      nobis adipisci eum minima desernut at porro, vertatis officia commodi itaque 
+                      vouptates vel suscipit assumenda soluta 
+                      ipsa voluptatibus laudantium labore harum?
+                    </p>
+        </div>
+    );
+}
+const c3Style = {
+    background: 'steelblue',
+    color: 'white',
+    padding: '1.5rem 1.5rem 5rem 1.5rem'
+};
